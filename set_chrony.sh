6c120035b300bc743240bd7e66d54f0314bc89c3
root <root@localhost.localdomain>
2018-03-31 13:06:06 +0900

    追加するサーバのリストえお書けるようにした。

--- set_chrony.sh
+++ set_chrony.sh
@@ -59,14 +59,13 @@ else
 	fi
 fi
 
-
 CONFIG_FILE="/etc/chrony.conf"
-
+SERVER_TEMP_ADD_IN="ntp_servers_add_in.txt"
+SERVER_TEMP_ADD_OUT="ntp_servers_add_out.txt"
 SERVER_TEMP="ntp_servers.txt"
 POOL_TEMP="ntp_pools.txt"
 MERGE_TEMP="ntp_sources_temp.txt"
 ADD_LIST="ntp_sources_list.txt"
-
 
 BACKUP=${CONFIG_FILE}.`date "+%Y%m%d_%H%M%S"`
 cp -p ${CONFIG_FILE} ${BACKUP}
@@ -85,23 +84,63 @@ if [ $? -gt 0 ]; then
    exit -1
 fi
 
-sed -n -e /^.*server.*iburst$/p ${CONFIG_FILE} | sort | uniq > ${SERVER_TEMP} && echo "server ntp.nict.jp iburst" >> ${SERVER_TEMP} && echo "server ntp1.jst.mfeed.ad.jp iburst" >> ${SERVER_TEMP}
+#追加したいntpサーバのリストをヒアドキュメントに記載する。
+HOGE=$(cat << EOS
+ntp.nict.jp
+ntp1.jst.mfeed.ad.jp
+s2csntp.miz.nao.ac.jp
+time.google.com
+ats1.e-timing.ne.jp
+ntp.dnsbalance.ring.gr.jp
+EOS
+)
+echo "${HOGE}" | sort | uniq > ${SERVER_TEMP_ADD_IN}
 if [ $? -gt 0 ]; then
-   echo "サーバリスト作成失敗。"
+   echo "現行サーバリスト取得失敗。"
+   exit -1
+fi
+
+#echo
+#cat ${SERVER_TEMP_ADD_IN}
+#echo
+
+NTP_SERVER_PREFIX="server"
+USE_IBURST="iburst"
+
+while read line
+do
+ echo "${NTP_SERVER_PREFIX} ${line} ${USE_IBURST}" >> ${SERVER_TEMP_ADD_OUT}
+ if [ $? -gt 0 ]; then
+    echo "追加サーバリスト作成失敗。"
+    exit -1
+ fi
+done < ${SERVER_TEMP_ADD_IN}
+#cat ${SERVER_TEMP_ADD_OUT}
+
+sed -n -e /^.*server.*iburst$/p ${CONFIG_FILE} | sort | uniq > ${SERVER_TEMP}
+if [ $? -gt 0 ]; then
+   echo "現行サーバリスト作成失敗。"
+   exit -1
+fi
+#cat ${SERVER_TEMP}
+
+cat ${SERVER_TEMP_ADD_OUT} >> ${SERVER_TEMP}
+if [ $? -gt 0 ]; then
+   echo "現行サーバリストへの追加サーバリストの追記失敗。"
    exit -1
 fi
 #cat ${SERVER_TEMP}
 
 sed -n -e /^.*pool.*iburst$/p ${CONFIG_FILE} | sort | uniq > ${POOL_TEMP}
 if [ $? -gt 0 ]; then
-   echo "プールリスト作成失敗。"
+   echo "現行プールリスト作成失敗。"
    exit -1
 fi
 #cat ${POOL_TEMP}
 
 touch ${MERGE_TEMP} && cat ${SERVER_TEMP} | sort | uniq >>${MERGE_TEMP} && cat ${POOL_TEMP} | sort | uniq >>${MERGE_TEMP}
 if [ $? -gt 0 ]; then
-   echo "マージ失敗。"
+   echo "サーバリスト、プールリストマージ、重複排除失敗。"
    exit -1
 fi
 #cat ${MERGE_TEMP}
